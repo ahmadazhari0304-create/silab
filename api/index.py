fac6e3268654f19d73f3c5d08bdc9b93555dae1f
xamiuz <102672118+xamiuz@users.noreply.github.com>
2026-08-27 17:22:32 +0800

    Explicitly assign top-level app in api/index.py

--- api/index.py
+++ api/index.py
@@ -4,5 +4,6 @@ import os
 # Tambahkan path root agar bisa import app.py dari folder utama
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Import object 'app' dari app.py
-from app import app
+# Import object 'app' dari app.py dan assign ke variable top-level 'app'
+import app as my_app
+app = my_app.app
