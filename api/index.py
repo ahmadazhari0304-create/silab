998b124a3c4fc8137a034bf10deda5f594c5108a
xamiuz <102672118+xamiuz@users.noreply.github.com>
2026-08-27 17:20:42 +0800

    Fix top-level app detection in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,8 @@
 import sys
 import os
-import traceback
 
+# Tambahkan path root agar bisa import app.py dari folder utama
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-try:
-    from app import app
-except Exception:
-    err = traceback.format_exc()
-    def app(environ, start_response):
-        status = '500 Internal Server Error'
-        response_headers = [('Content-type', 'text/html; charset=utf-8')]
-        start_response(status, response_headers)
-        return [f"<h1>Vercel Boot Exception:</h1><pre>{err}</pre>".encode('utf-8')]
+# Import object 'app' dari app.py
+from app import app
