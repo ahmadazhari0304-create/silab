fe6a80812ffb3252a791f24ffaa865f350d0571c
xamiuz <102672118+xamiuz@users.noreply.github.com>
2026-08-27 17:12:28 +0800

    Add raw WSGI exception catcher to api/index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,15 @@
-from app import app
+import sys
+import os
+import traceback
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+try:
+    from app import app
+except Exception:
+    err = traceback.format_exc()
+    def app(environ, start_response):
+        status = '500 Internal Server Error'
+        response_headers = [('Content-type', 'text/html; charset=utf-8')]
+        start_response(status, response_headers)
+        return [f"<h1>Vercel Boot Exception:</h1><pre>{err}</pre>".encode('utf-8')]
